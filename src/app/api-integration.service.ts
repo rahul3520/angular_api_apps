f7746d7347bb23933138cb241066142b04d098a0
rahulg3520 <rahul@nestgroup.net>
2022-11-30 00:20:19 +0530

    product api integration

--- src/app/api-integration.service.ts
+++ src/app/api-integration.service.ts
@@ -12,4 +12,9 @@ export class ApiIntegrationService {
   {
     return this.http.get("https://api.instantwebtools.net/v1/passenger?page=0&size=30")
   }
+
+  fetchProducts=() =>
+  {
+    return this.http.get("https://dummyjson.com/products")
+  }
 }
